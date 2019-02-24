37bf7c3b9409c622028f6d4ede499aae6fd6d5c5
Tilkeii <florian@gouarin.info>
2019-02-24 17:34:08 +0100

    change ladder position

--- Game.cpp
+++ Game.cpp
@@ -40,7 +40,13 @@ Game::Game()
 
 	for (int i = 0; i < ECHELLE_COUNT; i++)
 	{
-		sf::Vector2f pos = sf::Vector2f(100.f + 70.f * (i + 1), 0.f + BLOCK_SPACE * (i + 1) + sizeBlock.y);
+		sf::Vector2f pos;
+		if (i % 2 == 0) {
+			pos = sf::Vector2f(100.f + 70.f, 0.f + BLOCK_SPACE * (i + 1) + sizeBlock.y);
+		}
+		else {
+			pos = sf::Vector2f(100.f + 1010.f, 0.f + BLOCK_SPACE * (i + 1) + sizeBlock.y);
+		}
 		std::shared_ptr<Echelle> echelle = std::make_shared<Echelle>(textEchelle, pos);
 
 		EntityManager::m_Entities.push_back(echelle);
